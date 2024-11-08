253d1f000024dfb8189f6dbb0ffeeebf695bb6c4
Igor Prote <149106148+Igor-Prote@users.noreply.github.com>
2024-11-08 16:05:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
-<html lang="pt-bt">
+<html lang<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tela de login</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-    <header></header>
-    <main></main>
+    <div>
+        <h1>Login</h1>
+        <input type="text" placeholder="E-mail">
+        <br><br>
+        <input type="password" placeholder="Senha">
+        <br><br>
+        <button>Login</button>
+        <br><br>
+        <button>Registrar</button>
+    </div>
     <footer></footer>
     <?php
         
