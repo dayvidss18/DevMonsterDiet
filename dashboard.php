1aad1bfdff084054a97df4717fd002e3bc371c76
Dayvidss18 <dayvidss18@gmail.com>
2025-01-07 08:54:06 -0300

    Alteração de Dietas

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,8 @@
     }else{
         $logado = $_SESSION['email'];
         
+
+                
     }
 ?>
 <?php
@@ -54,11 +56,79 @@
         </div>
         <div class="dashboard_tela_secundario">
                 <div class="dashboard_tela_secundario_item">
+                        <h1>Dieta</h1>
+                        <?php 
+                        
+                        include_once('script.php');
+                        $dieta = '';
+                        $descriçãoDieta = '';
+                        if($usuario['dieta'] == 'nao'){
+                            $dieta = 'Dieta DinoCarnivora';
+                            $descriçãoDieta = 'A dieta "T-Rex" para humanos foca em alta ingestão de proteínas animais, com carnes magras (bife, frango, peixe) e ovos como base. É uma dieta de baixo carboidrato, priorizando gorduras saudáveis de fontes como abacate e óleos vegetais. Vegetais com baixo amido,
+                             como brócolis e espinafre, podem ser incluídos para fornecer fibras e micronutrientes. O foco é em alimentos naturais, com ênfase no aumento de massa muscular.';
 
+                            echo $dieta;
+                            echo "<br>";
+                            echo $descriçãoDieta;
+                        }elseif($usuario['dieta'] == 'vegetariano'){
+                            $dieta = 'Dieta Bronto';
+                            $descriçãoDieta = 'Baseada exclusivamente em alimentos de origem vegetal, exclui carnes, laticínios, ovos e mel.  
+                            **Alimentos Recomendados:** Leguminosas (feijões, lentilhas, grão-de-bico), cereais integrais, frutas, vegetais, oleaginosas (nozes, castanhas) e sementes (chia, linhaça).';
+
+                            echo $dieta;
+                            echo "<br>";
+                            echo $descriçãoDieta;
+                            
+                        }elseif($usuario['dieta'] == 'sim'){
+                            $dieta = 'Dieta triceratops';
+                            $descriçãoDieta = 'Baseada exclusivamente em alimentos de origem vegetal, exclui carnes, laticínios, ovos e mel.  
+                            **Alimentos Recomendados:** Leguminosas (feijões, lentilhas, grão-de-bico), cereais integrais, frutas, vegetais, oleaginosas (nozes, castanhas) e sementes (chia, linhaça).';
+
+                            echo $dieta;
+                            echo "<br>";
+                            echo $descriçãoDieta;
+                        }
+                        
+                        
+                        ?>
                 </div>
                 <div class="dashboard_tela_secundario_item1">
                     <h1>Recomendação</h1>
-                    <?php echo $usuario['recomendacao']; ?>
+                    <?php 
+                            include_once('script.php');
+                            $imc = $usuario['imc'];
+                            function recomendaRecomendacao(){
+                                global $imc, $recomendacao;
+                                if($imc < 18.5){
+                                    $recomendacao = 'Se o seu IMC está abaixo de 18,5, é importante ganhar peso de forma saudável.
+                                                     Aumente sua ingestão calórica com alimentos ricos em nutrientes, como nozes, abacates, carnes magras e carboidratos integrais.
+                                                     Coma de 5 a 6 refeições pequenas ao dia e inclua proteínas de alta qualidade (como ovos, peixes e legumes). Priorize gorduras saudáveis,
+                                                     como azeite de oliva e peixes gordurosos. Foque em musculação para ganhar massa muscular e evite exercícios aeróbicos excessivos. 
+                                                     Hidrate-se bem e, se necessário, considere suplementos como whey protein para complementar sua alimentação.';
+                                }elseif($imc >= 18.5 && $imc <= 24.9){
+                                    $recomendacao = 'Se o seu IMC está entre 18,5 e 24,9, você está no peso ideal. Para manter essa faixa, 
+                                                     mantenha uma alimentação equilibrada com porções adequadas de proteínas, carboidratos integrais e gorduras saudáveis. 
+                                                     Priorize alimentos frescos e nutritivos, como frutas, legumes, carnes magras e grãos integrais. Faça refeições regulares e inclua exercícios físicos,
+                                                     com foco em atividades aeróbicas e de resistência, para manter a saúde e o peso. Mantenha-se hidratado e evite excessos de alimentos processados e açúcares.';
+                                }elseif($imc >= 25.0 && $imc <= 29.9){
+                                    $recomendacao = 'Se o seu IMC está entre 25,0 e 29,9, você está na faixa de sobrepeso. Para melhorar sua saúde e alcançar um peso ideal, é importante adotar uma alimentação balanceada, 
+                                                     rica em vegetais, proteínas magras e carboidratos integrais. Evite alimentos ricos em gorduras saturadas e açúcares refinados. Pratique atividades físicas regulares, combinando exercícios aeróbicos e musculação,
+                                                     para queimar gordura e fortalecer os músculos. Mantenha-se hidratado e procure controlar o tamanho das porções para evitar o consumo excessivo de calorias.';
+                                }elseif($imc >= 30.0 && $imc <= 39.9){
+                                    $recomendacao = 'Se o seu IMC está entre 30,0 e 39,9, você está na faixa de obesidade. Para melhorar sua saúde e alcançar um peso ideal, é importante adotar uma alimentação balanceada, rica em vegetais, 
+                                                     proteínas magras e carboidratos integrais. Evite alimentos ricos em gorduras saturadas e açúcares refinados. Pratique atividades físicas regulares, combinando exercícios aeróbicos e musculação, para queimar gordura e fortalecer os músculos. 
+                                                     Mantenha-se hidratado e procure controlar o tamanho das porções para evitar o consumo excessivo de calorias.';
+                                }else{
+                                    $recomendacao = 'Se o seu IMC está acima de 39, você está na faixa de obesidade mórbida. Nessa situação, é crucial adotar uma abordagem cuidadosa e estruturada para a perda de peso. Priorize uma alimentação saudável, focando em alimentos ricos em nutrientes e com baixo teor calórico,
+                                                     como vegetais, proteínas magras e carboidratos integrais. Evite alimentos processados, fritos e ricos em açúcares. A prática regular de atividades físicas, com acompanhamento profissional, é fundamental, combinando exercícios aeróbicos e de força. Procure orientação médica e nutricional para garantir um plano de perda de peso seguro e eficaz,
+                                                     que pode incluir, em alguns casos, intervenções mais específicas como tratamentos médicos ou até cirurgias.';
+                                }
+                            }                                
+
+                            recomendaRecomendacao();
+                            echo $recomendacao;
+
+                    ?>
                 </div>
         </div>
     </div>
--- script.php
+++ script.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $senha1 = $_POST['senha1'];
     $senha2 = $_POST['senha2'];
     $senha = '';
-    $altura = $_POST['altura'];
+    $altura = (float) str_replace(',', '.', $_POST['altura']);
     $peso = $_POST['peso'];
     $sexo = $_POST['sexo'];
     $dieta = $_POST['dieta'] ?? 'Não';
@@ -40,41 +40,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $avaliacaoUsuario = "Obesidade Grave";
         }
     }
-    function recomendaRecomendacao(){
-        global $avaliacaoUsuario, $recomendacao;
-        if($avaliacaoUsuario == 'Extrema Magreza'){
-            $recomendacao = 'Se o seu IMC está abaixo de 18,5, é importante ganhar peso de forma saudável.
-             Aumente sua ingestão calórica com alimentos ricos em nutrientes, como nozes, abacates, carnes magras e carboidratos integrais.
-             Coma de 5 a 6 refeições pequenas ao dia e inclua proteínas de alta qualidade (como ovos, peixes e legumes). Priorize gorduras saudáveis,
-             como azeite de oliva e peixes gordurosos. Foque em musculação para ganhar massa muscular e evite exercícios aeróbicos excessivos. 
-             Hidrate-se bem e, se necessário, considere suplementos como whey protein para complementar sua alimentação.';
-        }elseif($avaliacaoUsuario == 'Normal'){
-            $recomendacao = 'Se o seu IMC está entre 18,5 e 24,9, você está no peso ideal. Para manter essa faixa, 
-             mantenha uma alimentação equilibrada com porções adequadas de proteínas, carboidratos integrais e gorduras saudáveis. 
-             Priorize alimentos frescos e nutritivos, como frutas, legumes, carnes magras e grãos integrais. Faça refeições regulares e inclua exercícios físicos,
-             com foco em atividades aeróbicas e de resistência, para manter a saúde e o peso. Mantenha-se hidratado e evite excessos de alimentos processados e açúcares.';
-        }elseif($avaliacaoUsuario == 'Sobrepeso'){
-            $recomendacao = 'Se o seu IMC está entre 25,0 e 29,9, você está na faixa de sobrepeso. Para melhorar sua saúde e alcançar um peso ideal, é importante adotar uma alimentação balanceada, 
-             rica em vegetais, proteínas magras e carboidratos integrais. Evite alimentos ricos em gorduras saturadas e açúcares refinados. Pratique atividades físicas regulares, combinando exercícios aeróbicos e musculação,
-             para queimar gordura e fortalecer os músculos. Mantenha-se hidratado e procure controlar o tamanho das porções para evitar o consumo excessivo de calorias.';
-        }elseif($avaliacaoUsuario == 'Obesidade'){
-            $recomendacao = 'Se o seu IMC está entre 25,0 e 29,9, você está na faixa de sobrepeso. Para melhorar sua saúde e alcançar um peso ideal, é importante adotar uma alimentação balanceada, rica em vegetais, 
-            proteínas magras e carboidratos integrais. Evite alimentos ricos em gorduras saturadas e açúcares refinados. Pratique atividades físicas regulares, combinando exercícios aeróbicos e musculação, para queimar gordura e fortalecer os músculos. 
-            Mantenha-se hidratado e procure controlar o tamanho das porções para evitar o consumo excessivo de calorias.';
-        }elseif($avaliacaoUsuario == 'Obesidade Grave'){
-            $recomendacao = 'Se o seu IMC está acima de 39, você está na faixa de obesidade mórbida. Nessa situação, é crucial adotar uma abordagem cuidadosa e estruturada para a perda de peso. Priorize uma alimentação saudável, focando em alimentos ricos em nutrientes e com baixo teor calórico,
-             como vegetais, proteínas magras e carboidratos integrais. Evite alimentos processados, fritos e ricos em açúcares. A prática regular de atividades físicas, com acompanhamento profissional, é fundamental, combinando exercícios aeróbicos e de força. Procure orientação médica e nutricional para garantir um plano de perda de peso seguro e eficaz,
-              que pode incluir, em alguns casos, intervenções mais específicas como tratamentos médicos ou até cirurgias.';
-        }
-    }
 
-    recomendaRecomendacao();
+
     verificaSenhaUsuario();
     verificaImc();
-
-    $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,idade,email,senha,altura,peso,sexo,imc,dieta,avaliacaoUsuario,recomendacao) 
-    VALUES('$nome','$idade','$email','$senha','$altura','$peso','$sexo','$imc','$dieta','$avaliacaoUsuario','$recomendacao')");
+    
+    $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,idade,email,senha,altura,peso,sexo,imc,dieta,avaliacaoUsuario) 
+    VALUES('$nome','$idade','$email','$senha','$altura','$peso','$sexo','$imc','$dieta','$avaliacaoUsuario')");
     header('Location: index.html');
     }
+
+
 ?>
 
